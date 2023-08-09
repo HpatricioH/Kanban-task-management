aac9e1e8281a09df93cfbd08023f3f539bc39348
HPHuerta <hhernanp@gmail.com>
2023-08-08 23:26:45 -0400

    feat: fixed tests

--- src/tests/createBoard.spec.ts
+++ src/tests/createBoard.spec.ts
@@ -1,12 +1,12 @@
-// import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-// test('Create Board', async ({ request }) => {
-//   const response = await request.post('https://kanban-task-management-api.vercel.app/api/v1/boards', {
-//     data: {
-//       name: 'Marketing Plan'
-//     }
-//   })
+test('Create Board', async ({ request }) => {
+  const response = await request.post('https://kanban-task-management-api.vercel.app/api/v1/boards', {
+    data: {
+      name: 'Marketing Plan'
+    }
+  })
 
-//   expect(response.status()).toBe(201)
-//   console.log(await response.json())
-// })
+  expect(response.status()).toBe(201)
+  console.log(await response.json())
+})
--- src/tests/deleteBoards.spec.ts
+++ src/tests/deleteBoards.spec.ts
@@ -1,8 +1,8 @@
-// import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-// test('Delete a board', async ({ request }) => {
-//   const response = await request.delete('http://localhost:8080/api/v1/boards/6481474a7442ad669c8b91ec')
+test('Delete a board', async ({ request }) => {
+  const response = await request.delete('http://localhost:8080/api/v1/boards/6481474a7442ad669c8b91ec')
 
-//   expect(response.status()).toBe(200)
-//   console.log(await response.json())
-// })
+  expect(response.status()).toBe(200)
+  console.log(await response.json())
+})
